ebf0ab72596d1232395a45722d0649c80596022c
oggunderscore <oggunderscore@gmail.com>
2020-04-05 23:18:30 -0700

    [fix] Made adjustments to HW12 in progress

--- HW12/hw12.cpp
+++ HW12/hw12.cpp
@@ -1,6 +1,61 @@
 #include "queue.h"
 
+//void q1Test(queue<class Type> q1) {
+    
+//}
+
 int main() {
     queue<int> q1;
+    queue<string> q2;
+    queue<double> q3;
 
+    system("clear");
+
+    cout << "-=-= Q1 =-=-\n" << endl;
+    cout << " > Initializing..." << endl;
+    q1.initialize();
+
+    cout << " > Attempting to dequeue..." << endl;
+    q1.dequeue();
+
+    cout << " > Queuing [6] numbers..." << endl;
+    q1.enqueue(2);
+    q1.enqueue(42);
+    q1.enqueue(123);
+    q1.enqueue(23);
+    q1.enqueue(6);
+    q1.enqueue(765);
+
+    cout << " > Printing numbers..." << endl;
+    q1.print();
+
+    cout << " > Dequeuing [4] numbers..." << endl;
+    q1.dequeue();
+    q1.dequeue();
+    q1.dequeue();
+    q1.dequeue();
+
+    cout << " > Printing numbers..." << endl;
+    q1.print();
+
+    cout << " > Queuing [5] numbers..." << endl;
+    q1.enqueue(23);
+    q1.enqueue(123);
+    q1.enqueue(25);
+    q1.enqueue(76);
+    q1.enqueue(12);
+
+    cout << " > Printing numbers..." << endl;
+    q1.print();
+
+    cout << " > Dequeuing [3] numbers..." << endl;
+    q1.dequeue();
+    q1.dequeue();
+    q1.dequeue();
+
+    cout << " > Printing numbers..." << endl;
+    q1.print();
+
+
+    return 0;
 }
