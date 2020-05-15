a69246d3eb2d17c7d17196ba4044ae264c9a64b7
oggunderscore <oggunderscore@gmail.com>
2020-05-15 13:40:49 -0700

    [fix] Test Commit

---
+++ Some New Folder/shapeEllipse.cpp
@@ -0,0 +1,69 @@
+#include "shapeEllipse.h"
+
+
+// Unknown variables at bottom of UML, just initializing in case? Probably not proper usage. 
+static double width = 0;
+static double height = 0;
+static QPoint location = null;
+
+    /// CONSTRUCTORS
+
+Ellipse::Ellipse(QPaintDedice device, int id) {
+    // TODO
+}
+
+    /// DESTRUCTORS
+
+Ellipse::~Ellipse() { // I think i forgot how to do a destructor xd
+
+}
+
+    // FUNCTIONS
+
+
+    // GETTERS
+
+void Ellipse::draw(QPaintDevice device) {
+
+}
+
+void Ellipse::move(int x, int y, int na) {
+
+}
+
+double Ellipse::perimeter() {
+
+}
+
+double Ellipse::area() {
+
+}
+
+double Ellipse::getWidth() {
+
+}
+
+double Ellipse::getHeight() {
+
+}
+
+QPoint Ellipse::getLocation() {
+
+}
+
+    // SETTERS
+
+void Ellipse::setDimentions(double w, double h) {
+    
+}
+
+void Ellipse::setLocation(int x, int y) {
+
+}
+
+// These two overload each other??
+
+void Ellipse::setLocation(QPoint pt) {
+
+}
+
